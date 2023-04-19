8501633628e72765ac1cd41d32e4ccffc9058a77
unknown <bucurtomaadrian@gmail.com>
2023-04-19 23:20:00 +0300

    confirmPassword not working

--- middlewares/validation/user.js
+++ middlewares/validation/user.js
@@ -1,11 +1,10 @@
-const { response } = require('express');
 const {check, validationResult} = require('express-validator');
 
 exports.validateUserSignUp = [    
     check('fullname')
     .trim()
     .not().isEmpty().withMessage('Name is required!')
-    .not().isString().withMessage('Must be a valid name!')
+    .isString().withMessage('Must be a valid name!')
     .isLength({min: 3, max: 25}).withMessage('Name must be between 3-25 characters!'),
 
     check('email')
@@ -17,14 +16,15 @@ exports.validateUserSignUp = [
     .not().isEmpty().withMessage('Password must not be empty!')
     .isLength({min: 6, max: 20}).withMessage('Password must be between 6-20 characters!'),
 
-    check('confirmPassword').trim().not().isEmpty()
-    .custom((value, {request}) => {
-        if(value !== request.body.password){
-            throw new Error("Passwords didn't match!")
-        }
-        return true;
-    }).withMessage("Passwords didn't match")
-
+    // check('confirmPassword')
+    // .trim()
+    // .not().isEmpty()
+    // .custom((value, {request}) => {
+    //     if(value !== request.body.password){
+    //         throw new Error("Both passwords must be the same!")
+    //     }
+    //     return true;
+    // }).withMessage("Passwords didn't match")
 ];
 
 exports.userValidation = (request, response, next) => {
